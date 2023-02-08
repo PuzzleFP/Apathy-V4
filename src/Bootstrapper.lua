789eb96df2914cba320a405096c0b64549efdf52
Infinitus <82955239+PuzzleFP@users.noreply.github.com>
2023-02-08 03:56:52 +0000

    Update and rename Bootstrapper to Bootstrapper.lua

---
+++ src/Bootstrapper.lua
@@ -0,0 +1,26 @@
+getgenv().RequestServer = function(Type, Data)
+    local Package = {
+        Url = "http://localhost:8090/",
+        Headers = {
+            InstructionType = Type, 
+            InstructionData = Data}
+    }
+
+    return HTTP(Package)
+end 
+
+
+getgenv().GetScript = function(Name, LoadIntoGlobalEnvironment)
+	local Script = RequestServer("GetScript", Name)
+
+	if not Script.Body then 
+		error(string.format("Failed to get script: %s", Name))
+	end 
+
+	if LoadIntoGlobalEnvironment then 
+		getgenv()[Name] = loadstring(Script.Body)()
+		return getgenv()[Name]
+	end 
+
+	return loadstring(Script.Body)()
+end 
