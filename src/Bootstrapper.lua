fb7212311740a6f54c75bccca8d83ce91b591c69
Infinitus <82955239+PuzzleFP@users.noreply.github.com>
2023-02-08 14:13:36 +0000

    Will be revamped with rostruct

--- src/Bootstrapper.lua
+++ src/Bootstrapper.lua
@@ -9,20 +9,5 @@ getgenv().RequestServer = function(Type, Data)
     return HTTP(Package)
 end 
 
-
-getgenv().GetScript = function(Name, LoadIntoGlobalEnvironment)
-	local Script = RequestServer("GetScript", Name)
-
-	if not Script.Body then 
-		error(string.format("Failed to get script: %s", Name))
-	end 
-
-	if LoadIntoGlobalEnvironment then 
-		getgenv()[Name] = loadstring(Script.Body)()
-		return getgenv()[Name]
-	end 
-
-	return loadstring(Script.Body)()
-end 
-
+local Apathy = RequestServer("GetApathy")
 local Carbon = loadstring(game:HttpGet("https://raw.githubusercontent.com/WoffleTbh/CarbonUI/main/carbonui.lua"))()
