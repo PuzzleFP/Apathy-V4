e567319b23d3043d96fcf5f6e30c263bdac8e0b5
Infinitus <82955239+PuzzleFP@users.noreply.github.com>
2023-03-13 20:41:24 +0000

    Update FakeCharacter.lua

--- src/Utilities/FakeCharacter.lua
+++ src/Utilities/FakeCharacter.lua
@@ -1,1 +1,34 @@
+local StarterPlayer = game:GetService("StarterPlayer")
+local Players = game:GetService("Players")
 
+local Client = Players.LocalPlayer
+
+local FakeCharacterController = {}
+FakeCharacterController.__index = FakeCharacterController
+
+function FakeCharacterController.New()
+    local self = setmetatable({}, FakeCharacterController)
+
+    -- Check if there is an existing character template that can be used to form the fake character
+    -- Can be the starter character, or the current character
+    -- TODO: Build a new character from scratch, and not rely on templates?
+
+    -- Grab the character template, and clone it
+
+    local CharacterTemplate = 
+
+    self.CurrentCharacterState = false 
+
+    return self 
+end 
+
+function FakeCharacterController:SwitchToFakeCharacter()
+    
+end 
+
+function FakeCharacterController:SwitchToRealCharacter()
+
+end 
+ 
+
+return FakeCharacterController 
